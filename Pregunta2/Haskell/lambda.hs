804210e1d10b7967850bbc2d04d6dc9289d1e7dd
willson97 <104342529+G00F7@users.noreply.github.com>
2022-06-15 13:23:57 -0400

    Update lambda.hs

--- Pregunta2/Haskell/lambda.hs
+++ Pregunta2/Haskell/lambda.hs
@@ -4,12 +4,13 @@ fibo 2 = 1
 fibo 3 = 1
 fibo n | n > 3 = fibo(n-1) + fibo(n-2) + fibo(n-3)
 
-fibonacci :: Int -> [Int]
-fibonacci n = map (\x -> fibo(x))[1..n-1]
+lambda :: Int -> [Int]
+lambda n = map (\x -> fibo(x))[1..n]
 
 main = do
  putStrLn "\nFibonacci Funciones Temporales(lambda)"
  putStrLn "Introduzca un N : "
- n<-getLine
+ entrada<-getLine
+ let n = read entrada :: Int
  putStrLn "Los N primeros fibonacci "
- print(fibonacci(n))
+ print(lambda(n))
